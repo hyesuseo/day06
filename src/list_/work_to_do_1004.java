a2c0837d493e40b854dd46ecce3492ea0a44b11f
aaa <aaa@aaa.com>
2024-10-04 17:53:03 +0900

    1004 work

---
+++ src/list_/work_to_do_1004.java
@@ -0,0 +1,12 @@
+package list_;
+
+public class work_to_do_1004 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ArrayList 정리하고 실습해보기 - 퀴즈 풀던거 마저 풀기
+		// 다음에 나갈 진도 예습하기
+		//키보드 갖고오기!!!!!
+	}
+
+}
